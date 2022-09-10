9ada08fc07c65fb49d1ae6ec1ec29bcc2c99d8c9
Beren Erchamion <beren@houseofbeor.net>
2022-09-10 14:14:38 -0400

    Added transaction code - self hosted ethereum dev instance

---
+++ index.js
@@ -0,0 +1,30 @@
+const Web3 = require('web3')
+const EthereumTransaction = require('ethereumjs-tx').Transaction
+
+// Self hosted ganache ethereum blockchain
+const url = 'HTTP://127.0.0.1:7545'
+const sendingAddress = '0xaA279Edb9741681c4E3Ff3821fEa3801B6Ce9e12'
+const receivingAddress = '0x9aa1e39998DF63A84052D9ed88Fea6362c4Da143'
+
+var web3 = new Web3(url)
+
+var rawTransaction = {
+  nonce: web3.utils.toHex(1),
+  to: receivingAddress,
+  gasPrice: web3.utils.toHex(20000000),
+  gasLimit: web3.utils.toHex(30000),
+  value: web3.utils.toHex(100),
+  data: web3.utils.toHex('')
+}
+
+const privateKeySender = 'put a private key here'
+const privateKeySenderHex = Buffer.from(privateKeySender, 'hex')
+var transaction = new EthereumTransaction(rawTransaction)
+transaction.sign(privateKeySenderHex)
+
+var serializedTransaction = transaction.serialize()
+web3.eth.sendSignedTransaction(serializedTransaction)
+
+console.log('Transaction Sent')
+web3.eth.getBalance(sendingAddress).then(console.log)
+web3.eth.getBalance(receivingAddress).then(console.log)
